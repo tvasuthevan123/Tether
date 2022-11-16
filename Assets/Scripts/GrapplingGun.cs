34a5c8ff724bac5ade73a3188255f20495de1796
cf3g19 <56079097+cf3g19@users.noreply.github.com>
2022-11-16 18:57:35 +0000

    Change player collider, add incomplete reel

--- Assets/Scripts/GrapplingGun.cs
+++ Assets/Scripts/GrapplingGun.cs
@@ -6,17 +6,38 @@ public class GrapplingGun : MonoBehaviour
     private LineRenderer lr;
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
-    public Transform gunTip, camera, player;
+    public Transform gunTip, camera;
     public float maxDistance = 100f;
+    
     private SpringJoint joint;
+    public float reelAccel = 5f;
+    private bool isReeling = false; 
+
+    private Vector3 currentGrapplePosition;
+
+    public GameObject player;
+    public Rigidbody playerRb;
 
     void Awake()
     {
         lr = GetComponent<LineRenderer>();
+        playerRb = player.GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        if(IsGrappling() && isReeling)
+        {
+            // reelAmount = reelAccel*reelAmount;
+            // joint.minDistance =- reelAmount;
+            // Debug.Log("Reel Amount = " + reelAmount);
+            // Debug.Log("Joint Max Distance = " + joint.minDistance);
+            Vector3 direction = Vector3.MoveTowards(player.transform.position, grapplePoint, reelAccel * Time.deltaTime);
+            playerRb.MovePosition(direction);
+            playerRb.useGravity = false;
+        }
+        // TODO: Possible refactor using input system onPress?
+        // Grapple
         if (Input.GetMouseButtonDown(0))
         {
             StartGrapple();
@@ -24,6 +45,16 @@ public class GrapplingGun : MonoBehaviour
         else if (Input.GetMouseButtonUp(0))
         {
             StopGrapple();
+        }
+        
+        // Reel button interaction
+        if (Input.GetMouseButtonDown(1))
+        {
+            StartReel();
+        }
+        else if(Input.GetMouseButtonUp(1))
+        {
+            StopReel();
         }
     }
 
@@ -46,7 +77,7 @@ public class GrapplingGun : MonoBehaviour
             joint.autoConfigureConnectedAnchor = false;
             joint.connectedAnchor = grapplePoint;
 
-            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+            float distanceFromPoint = Vector3.Distance(player.transform.position, grapplePoint);
 
             //The distance grapple will try to keep from grapple point. 
             joint.maxDistance = distanceFromPoint * 0.8f;
@@ -62,7 +93,6 @@ public class GrapplingGun : MonoBehaviour
         }
     }
 
-
     /// <summary>
     /// Call whenever we want to stop a grapple
     /// </summary>
@@ -72,7 +102,17 @@ public class GrapplingGun : MonoBehaviour
         Destroy(joint);
     }
 
-    private Vector3 currentGrapplePosition;
+    void StartReel()
+    {
+        isReeling=true;
+    }
+
+    void StopReel()
+    {
+        isReeling=false;
+        // playerRb.velocity = Vector3.zero;
+        playerRb.useGravity = true;
+    }
 
     void DrawRope()
     {
